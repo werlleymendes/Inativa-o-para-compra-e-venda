52e83fede4e06805c6d84dc99c43d3ca3ac6afb5
Werley M Andrade <werlleymendes@hotmail.com>
2024-03-07 11:07:35 -0300

    Add arquivo inativacao

---
+++ InativacaoCompra.sql
@@ -0,0 +1,14 @@
+SELECT * FROM consinco.mrl_prodempseg
+
+SELECT a.seqproduto, b.estqloja, b.estqtroca
+       FROM consinco.map_produto        a
+       JOIN consinco.mrl_produtoempresa b
+            ON a.seqproduto = b.seqproduto
+       WHERE b.estqloja          <= 0 and
+             b.estqtroca         <= 0 and
+             b.nroempresa        = 1  and
+             b.statuscompra      = 'A'  and
+             b.qtdpendpedcompra  = 0  and
+             b.dtaultmovtacao    < trunc(sysdate) - 540;
+             
+            
