233734cbeb85a74ef2c5b43d70a584a3226bab87
Werley M Andrade <werlleymendes@hotmail.com>
2024-03-11 11:18:35 -0300

    Inclui variável para seleção de empresa

--- InativacaoCompra.sql
+++ InativacaoCompra.sql
@@ -1,10 +1,11 @@
-UPDATE consinco.map_produto        a 
+UPDATE consinco.mrl_produtoempresa
        SET b.statuscompra = 'I'
-       JOIN consinco.mrl_produtoempresa b
+       FROM consinco.mrl_produtoempresa b
+       JOIN consinco.map_produto        a       
             ON a.seqproduto = b.seqproduto
        WHERE b.estqloja          <= 0 and
              b.estqtroca         <= 0 and
-             b.nroempresa        = 1  and
+             b.nroempresa        = &empresa  and
              b.statuscompra      = 'A'  and
              b.qtdpendpedcompra  = 0  and
              b.dtaultmovtacao    < trunc(sysdate) - 540;
