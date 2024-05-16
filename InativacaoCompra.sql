eeeaefc8d927db4016dc243b49c6c9bef7106851
Werley M Andrade <werlleymendes@hotmail.com>
2024-05-16 17:12:03 -0300

    alterações na query Inativação de Compra

--- InativacaoCompra.sql
+++ InativacaoCompra.sql
@@ -1,13 +1,9 @@
 UPDATE consinco.mrl_produtoempresa
        SET b.statuscompra = 'I'
-       FROM consinco.mrl_produtoempresa b
-       JOIN consinco.map_produto        a       
-            ON a.seqproduto = b.seqproduto
        WHERE b.estqloja          <= 0 and
              b.estqtroca         <= 0 and
              b.nroempresa        = &empresa  and
              b.statuscompra      = 'A'  and
              b.qtdpendpedcompra  = 0  and
              b.dtaultmovtacao    < trunc(sysdate) - 540;
-             
 
